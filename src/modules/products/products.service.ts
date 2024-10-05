cb77b41cfb5a9883de28ab35d744045566fe4c78
ghulam rasool <grsaddiqui75@gmail.com>
2024-10-05 11:31:39 +0500

    sff pull

--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -1,14 +1,14 @@
-import { Injectable } from '@nestjs/common';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
-import { PRODUCT_MODEL, PRODUCT_TYPE } from 'src/schema/product.schema';
-import { Model } from 'mongoose';
-import { InjectModel } from '@nestjs/mongoose';
+import { Injectable } from "@nestjs/common";
+import { CreateProductDto } from "./dto/create-product.dto";
+import { UpdateProductDto } from "./dto/update-product.dto";
+import { PRODUCT_MODEL, PRODUCT_TYPE } from "src/schema/product.schema";
+import { Model } from "mongoose";
+import { InjectModel } from "@nestjs/mongoose";
 
 @Injectable()
 export class ProductsService {
   constructor(
-    @InjectModel(PRODUCT_MODEL) private productModel: Model<PRODUCT_TYPE>,
+    @InjectModel(PRODUCT_MODEL) private productModel: Model<PRODUCT_TYPE>
   ) {}
   async create(createProductDto: CreateProductDto) {
     const product = await this.productModel.create(createProductDto);
@@ -29,7 +29,7 @@ export class ProductsService {
     let product = await this.productModel.findByIdAndUpdate(
       id,
       { ...updateProductDto },
-      { new: true },
+      { new: true }
     );
     if (!product) {
       return null;
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -6,46 +6,60 @@ import {
   Patch,
   Param,
   Delete,
-} from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+} from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
 
-@Controller('users')
+@Controller("users")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return {
+      message: "user create successfully",
+      success: true,
       data: this.usersService.create(createUserDto),
     };
   }
 
   @Get()
-  findAll() {
+  async findAll() {
+    const user = await this.usersService.findAll();
     return {
-      data: this.usersService.findAll(),
+      message: "user find all successfully",
+      success: true,
+      data: {
+        length: user.length,
+        user: user,
+      },
     };
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(":id")
+  findOne(@Param("id") id: string) {
     return {
+      message: "user by id successfully",
+      success: true,
       data: this.usersService.findOne(id),
     };
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateUserDto: UpdateUserDto) {
     return {
+      message: "user update successfully",
+      success: true,
       data: this.usersService.update(id, updateUserDto),
     };
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  remove(@Param("id") id: string) {
     return {
+      message: "user delet successfully",
+      success: true,
       data: this.usersService.remove(id),
     };
   }
